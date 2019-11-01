3a54a23d187c3b72586aca2d4a4f5e6fc416bb82
Ivan Biryuchev <ivanbiryuchev@coherentsolutions.com>
2019-11-01 17:17:35 +0300

    Fix utils

---
+++ src/main/java/bsuir/ai/sostis/utils/IndexUtil.java
@@ -0,0 +1,11 @@
+package bsuir.ai.sostis.utils;
+
+import bsuir.ai.sostis.model.Document;
+
+public class IndexUtil {
+
+    // TODO: add indexing from first lab. So we might need to use db.
+    public static Document indexDocument(Document document) {
+        return document;
+    }
+}
--- src/main/java/bsuir/ai/sostis/utils/TextUtils.java
+++ src/main/java/bsuir/ai/sostis/utils/TextUtils.java
@@ -3,7 +3,6 @@ package bsuir.ai.sostis.utils;
 import bsuir.ai.sostis.model.Document;
 import bsuir.ai.sostis.model.Sentence;
 import bsuir.ai.sostis.model.Word;
-import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -12,7 +11,6 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@Component
 public class TextUtils {
 
     private static final Pattern SENTENCE_PATTERN = Pattern.compile("[A-ZА-Я]+[A-Za-zА-Яа-я0-9,;'\"\\s]*[.?!]\\s*");
@@ -29,11 +27,13 @@ public class TextUtils {
             sentences.add(sentence);
         }
 
-        return Document.builder()
-                .text(docText)
-                .sentences(sentences)
-                .title(docTitle)
-                .build();
+        return IndexUtil.indexDocument(
+                Document.builder()
+                        .text(docText)
+                        .sentences(sentences)
+                        .title(docTitle)
+                        .build()
+        );
     }
 
     private static Sentence createSentence(int sentenceNumber, String sentenceText) {
@@ -51,7 +51,6 @@ public class TextUtils {
                 .build();
     }
 
-    // TODO: need to add word indexing somewhere
     private static Word createWord(int wordNumber, String wordText) {
         return Word.builder()
                 .number(wordNumber)
